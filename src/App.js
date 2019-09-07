a64eb069992d992f45a27d6bedcf9f08dfae0c01
Mahdieh <mahdieh.jamshidi69@gmail.com>
2019-09-07 15:38:39 +0430

    some change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
+import Component1 from "./_components/Component1";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <h1>hallo</h1>
-      </header>
-    </div>
-  );
-  return 
-    
-   
-       
-  
+  return <Component1/>;
 }
+
+
+
 
 export default App;
