cf5dea5cb0fece4b9b06dea36022901b24a7fd92
shirinbano <shirinkhorasanian@yahoo.com>
2019-09-08 18:12:00 +0430

    router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import AntTab from './_component/admin';
+
 class App extends Component {
    constructor(){
        super();
@@ -19,12 +22,15 @@ class App extends Component {
          }
    render() {
         return (
+            <Router>
             <div className="App">
                 <h1>Users</h1>
                 {this.state.users.map(user =>
                 <div key={user.id}>user: {user.name} Password: {user.password}</div>
               )}
             </div>
+            <Route path="/admin" exact component={AntTab} />
+           </Router>
         );
     }
 }
